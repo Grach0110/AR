2c3e50720a6e2777f3a0effc6192bc7a2d556831
Grach <Pavlov-Maxim.S@yandex.ru>
2020-11-17 20:09:14 +0300

    Дополнение

---
+++ Assets/PlaceOnPlane_Script.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlaceOnPlane_Script : MonoBehaviour
+{
+    ARRaycastManager aRRaycastManager;
+    private Vector2 touchPosition;
+
+    public GameObject scenePrefab;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();
+        scenePrefab.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+
+            if (aRRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+            {
+                var hitPose = hits[0].pose;
+
+                scenePrefab.SetActive(true);
+                scenePrefab.transform.position = hitPose.position;
+                LookAtPlayer(scenePrefab.transform);
+            }
+        }
+    }
+
+    void LookAtPlayer(Transform scene)
+    {
+        var lookDirection = Camera.main.transform.position - scene.position;
+        lookDirection.y = 0f;
+        scene.rotation = Quaternion.LookRotation(lookDirection);
+    }
+}
---
+++ Assets/UX_Controller_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class UX_Controller_Script : MonoBehaviour
+{
+    private ARPlaneManager aRPlaneManager;
+    public GameObject handAnim;
+
+
+    private void Start()
+    {
+        aRPlaneManager = GetComponent<ARPlaneManager>();
+        aRPlaneManager.planesChanged += aRPlaneManager_planesChanged;
+    }
+
+    void aRPlaneManager_planesChanged(ARPlanesChangedEventArgs obj)
+    {
+        foreach (var item in obj.added)
+        {
+            handAnim.SetActive(false);
+            break;
+        }
+    }
+}
