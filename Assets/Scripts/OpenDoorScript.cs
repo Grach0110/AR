da9ac2adbd9a55c15934b68cc6f8d2c183de456a
Grach <Pavlov-Maxim.S@yandex.ru>
2020-11-27 20:33:15 +0300

    Готовая сцена
    Мир загружается, дверь открывается.

--- Assets/Scripts/OpenDoorScript.cs
+++ Assets/Scripts/OpenDoorScript.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.XR.ARFoundation;
 
 public class OpenDoorScript : MonoBehaviour, IPointerClickHandler
 {
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        var animator = GetComponent<Animator>();
-        animator.SetTrigger("OpenDoor");
+        GetComponent<Animator>().SetTrigger("OpenDoor");
+
+        var ARPlaneManager = FindObjectOfType<ARPlaneManager>();
+        var ARPointManager = FindObjectOfType<ARPointCloudManager>();
+
+        foreach (var point in ARPointManager.trackables)
+        {
+            point.gameObject.SetActive(false);
+        }
+
+        foreach (var plane in ARPlaneManager.trackables)
+        {
+            plane.gameObject.SetActive(false);
+        }
+
+        ARPlaneManager.enabled = false;
+        ARPointManager.enabled = false;
     }
 }
